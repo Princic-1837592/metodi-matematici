5dee8892c5b57c4445ac208ee2be1e7f4ae387e3
Princic-1837592 <andreapri.99@gmail.com>
2024-12-30 12:52:12 +0100

    esame 18 dicembre 2024

--- compile-esame.sh
+++ compile-esame.sh
@@ -1,3 +1,3 @@
-typst compile metodi-matematici-latex/typst/$1.typ esami/$1.pdf --font-path metodi-matematici-latex/typst/Latin-Modern-Roman
+typst compile metodi-matematici-latex/esami/$1.typ esami/$1.pdf --font-path metodi-matematici-latex/typst/Latin-Modern-Roman --root metodi-matematici-latex
 SumatraPDF esami/$1.pdf &
-typst watch metodi-matematici-latex/typst/$1.typ esami/$1.pdf --font-path metodi-matematici-latex/typst/Latin-Modern-Roman
+typst watch metodi-matematici-latex/esami/$1.typ esami/$1.pdf --font-path metodi-matematici-latex/typst/Latin-Modern-Roman --root metodi-matematici-latex
